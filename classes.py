ae3cd993df0ffa0be2b5d7cec74ec71dd161e4c4
mavi <mavisberrye@gmail.com>
2026-05-13 12:52:40 -0300

    Implementação do método empréstimo e do erro de input inválido

--- classes.py
+++ classes.py
@@ -1,7 +1,10 @@
 from exceptions import ContaZeradaError, SaldoInsuficienteError
+from uteis import verificanumero
 class Contabancaria:
     def __init__(self, saldo):
         self.saldo = saldo
+        if verificanumero(saldo) == False:
+            print("O saldo deve ser um valor numérico!!")
 
 class corrente(Contabancaria):
     def __init__(self, saldo):
@@ -12,7 +15,13 @@ class corrente(Contabancaria):
             raise ContaZeradaError("Você não pode realizar esse saque, pois, sua conta está zerada!!")
         elif self.saldo < valor:
             raise SaldoInsuficienteError("Parece que você não tem saldo suficiente para realizar esse saque!")
+        elif verificanumero(valor) == False:
+            print("O saque deve ser um valor numérico!!")
         else:
             self.saldo -= valor
             print(f"Saque realizado com sucesso!! Agora seu saldo é de {self.saldo}")
         return self.saldo
+    def emprestimo(self, valor):
+        self.valor = valor
+        self.saldo += valor
+        print(f"Empréstimo de {self.valor} bem sucedido! Agora sua conta possui R${self.saldo}!")
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from exceptions import ContaZeradaError, SaldoInsuficienteError
 from classes import corrente, Contabancaria
 
-conta = corrente(200)
+conta = corrente(100)
+
+conta.emprestimo(0)
 
 try: 
-    conta.sacar(101)
+    conta.sacar(20)
 except ContaZeradaError as erro:
     print(erro)
 except SaldoInsuficienteError as erro:
